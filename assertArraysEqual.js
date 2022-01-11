4d86a65889a5be57b315fc252927cceb60a9f551
XiaoMaShawn <ma2014xiao@hotmail.com>
2022-01-11 17:46:17 -0500

    add the assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,30 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const eqArrays = function (arr1, arr2) {
+  let result;
+  let output;
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      result = assertEqual(arr1[i], arr2[i]);
+      if (result === false) {
+        output = `Assertion Failed: ${arr1} !== ${arr2}`;
+        break;
+      } else {
+        output = `Assertion Passed: ${arr1} === ${arr2}`;
+      }
+    }
+    console.log(output);
+  }
+}
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
