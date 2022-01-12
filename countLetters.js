793b498fff9c8dd31a4d73df2f5693d90d15b518
XiaoMaShawn <ma2014xiao@hotmail.com>
2022-01-12 16:19:42 -0500

    create the countLetter.js

---
+++ countLetters.js
@@ -0,0 +1,27 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log('true');
+  } else {
+    console.log('false');
+  }
+};
+
+const countLetters = function (strings) {
+  // set a result which is an object
+  let result = {};
+  // loop through the strings
+  for (let letter of strings) {
+    //chech whether the letter is a space
+    if (letter !== ' ') {
+      // check whether there is the same property in the result object
+      if (!result[letter]) {
+        result[letter] = 1;
+      } else {
+        result[letter] += 1;
+      }
+    }
+  }
+  return result;
+}
+
+console.log(countLetters("lighthouse in the house"));
